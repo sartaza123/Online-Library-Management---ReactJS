d69572ca6a5b9195ab272399a7bb21bcd4a5a90b
sartaza123 <sartaza123@gmail.com>
2026-02-03 13:16:46 +0530

    Search is added for search book by their title or author name both

--- Online-Library-Management/src/Components/BrowseBook.jsx
+++ Online-Library-Management/src/Components/BrowseBook.jsx
@@ -2,17 +2,25 @@ import { useParams } from "react-router-dom";
 import books from "../Utils/data";
 import Category from "./Category";
 import BookList from "./BookList";
+import { useState } from "react";
 
 function BrowseBook() {
   const { category } = useParams();
+  const [searchText, setSearchText] = useState("");
 
-  const filteredBooks =
-    !category || category === "all"
-      ? books
-      : books.filter((book) => book.category === category);
+  const filteredBooks = books.filter((book) => {
+    const matchCategory =
+      !category || category === "all" || book.category === category;
+
+    const matchSearch =
+      book.title.toLowerCase().includes(searchText.toLowerCase()) ||
+      book.author.toLowerCase().includes(searchText.toLowerCase());
+
+    return matchCategory && matchSearch;
+  });
   return (
     <div className="mt-24">
-      <Category />
+      <Category searchText={searchText} setSearchText={setSearchText} />
       <BookList books={filteredBooks} />
     </div>
   );
--- Online-Library-Management/src/Components/Category.jsx
+++ Online-Library-Management/src/Components/Category.jsx
@@ -1,17 +1,17 @@
 import { NavLink } from "react-router-dom";
 import books from "../Utils/data";
 
-function Category() {
+function Category({ searchText, setSearchText }) {
   const categories = ["all", ...new Set(books.map((book) => book.category))];
   return (
-    <>
-      <ul className="flex gap-4 flex-wrap m-5">
+    <div className="flex justify-between m-5">
+      <ul className="flex gap-4 flex-wrap">
         <h2 className="text-xl font-bold mr-8">Category : </h2>
         {categories.map((cate) => {
           return (
             <li
               key={cate}
-              className="mr-10 capitalize text-xl hover:text-green-800 hover:scale-105 transition-all"
+              className="mr-4 capitalize text-xl hover:text-green-800 hover:scale-105 transition-all"
             >
               <NavLink
                 to={cate === "all" ? "/browse" : `/browse/${cate}`}
@@ -26,7 +26,17 @@ function Category() {
           );
         })}
       </ul>
-    </>
+      <div className="flex gap-4 mr-5">
+        <input
+          type="text"
+          name="search"
+          placeholder="Search: Book or Author"
+          className="border rounded-md px-3 w-60 h-8"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+      </div>
+    </div>
   );
 }
 export default Category;
