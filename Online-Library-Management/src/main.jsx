923bdddca430e480d4b31d045e1134f68995ad93
sartaza123 <sartaza123@gmail.com>
2026-02-05 17:24:54 +0530

    Lazyloading is added to load the on demand component

--- Online-Library-Management/src/main.jsx
+++ Online-Library-Management/src/main.jsx
@@ -1,43 +1,72 @@
-import { StrictMode } from "react";
+import { StrictMode, Suspense } from "react";
+import { lazy } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import App from "./App.jsx";
-import Error from "./Components/Error.jsx";
-import BrowseBook from "./Components/BrowseBook.jsx";
-import AddBook from "./Components/AddBook.jsx";
-import Home from "./Components/Home.jsx";
-import BookDetails from "./Components/BookDetails.jsx";
+const App = lazy(() => import("./App.jsx"));
+const Error = lazy(() => import("./Components/Error.jsx"));
+const BrowseBook = lazy(() => import("./Components/BrowseBook.jsx"));
+const AddBook = lazy(() => import("./Components/AddBook.jsx"));
+const Home = lazy(() => import("./Components/Home.jsx"));
+const BookDetails = lazy(() => import("./Components/BookDetails.jsx"));
 import { Provider } from "react-redux";
 import appStore from "./Utils/appStore";
 
 const appRouter = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
-    errorElement: <Error />,
+    element: (
+      <Suspense fallback={<div>Loading...</div>}>
+        <App />
+      </Suspense>
+    ),
+    errorElement: (
+      <Suspense fallback={<div>Loading...</div>}>
+        <Error />
+      </Suspense>
+    ),
     children: [
       {
         path: "/",
-        element: <Home />,
+        element: (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Home />
+          </Suspense>
+        ),
       },
       {
         path: "/browse",
-        element: <BrowseBook />,
+        element: (
+          <Suspense fallback={<div>Loading...</div>}>
+            <BrowseBook />
+          </Suspense>
+        ),
       },
       {
         path: "/browse/:category",
-        element: <BrowseBook />,
+        element: (
+          <Suspense fallback={<div>Loading...</div>}>
+            <BrowseBook />
+          </Suspense>
+        ),
       },
       {
         path: "/add",
-        element: <AddBook />,
+        element: (
+          <Suspense fallback={<div>Loading...</div>}>
+            <AddBook />
+          </Suspense>
+        ),
       },
     ],
   },
   {
     path: "/details/:id",
-    element: <BookDetails />,
+    element: (
+      <Suspense fallback={<div>Loading...</div>}>
+        <BookDetails />
+      </Suspense>
+    ),
   },
 ]);
 
